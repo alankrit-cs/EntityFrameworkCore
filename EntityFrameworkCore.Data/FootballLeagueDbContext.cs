2abbc507b597cd3937d18f5a505e0810df13870a
alankrit-cs <108877396+alankrit-cs@users.noreply.github.com>
2025-06-27 15:46:56 +0530

    Add project files.

---
+++ EntityFrameworkCore.Data/FootballLeagueDbContext.cs
@@ -0,0 +1,40 @@
+﻿using EntityFrameworkCore.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCore.Data
+{
+    public class FootballLeagueDbContext : DbContext
+    {
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Coach> Coaches { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source= FootballLeague_EfCore.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Team>().HasData(
+                new Team
+                {
+                    TeamId = 1,
+                    Name = "India",
+                    CreatedDate = DateTime.UtcNow
+                },
+                new Team
+                {
+                    TeamId = 2,
+                    Name = "Protugal",
+                    CreatedDate = DateTime.UtcNow
+                },
+                new Team
+                {
+                    TeamId = 3,
+                    Name = "Brazil",
+                    CreatedDate = DateTime.UtcNow
+                }
+            );
+        }
+    }
+}
