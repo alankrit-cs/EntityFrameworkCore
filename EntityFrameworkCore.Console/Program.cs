4854b36128299ca5111671a2fe0cd2a4bf403c31
alankrit-cs <108877396+alankrit-cs@users.noreply.github.com>
2025-06-30 20:39:34 +0530

    Querying the DB with LINQ

--- EntityFrameworkCore.Console/Program.cs
+++ EntityFrameworkCore.Console/Program.cs
@@ -1,2 +1,131 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EntityFrameworkCore.Data;
+using EntityFrameworkCore.Domain;
+using Microsoft.EntityFrameworkCore;
+
+using var context = new FootballLeagueDbContext();
+
+
+GetAllTeams();
+QuerySingleRecords();
+FilterMultipleRecords();
+SearchWithWildCard();
+AggregateFunctions();
+GroupByItems();
+OrderByItems();
+SelectAndProjections();
+
+async Task SelectAndProjections()
+{
+    var teams = await context.Teams
+        .Select(q => q.Name)
+        .ToListAsync();
+
+    foreach (var name in teams)
+    {
+        Console.WriteLine(name);
+    }
+
+    var teamsWithDate = await context.Teams
+        .Select(q => new { q.Name, q.CreatedDate })
+        .ToListAsync();
+
+    foreach (var team in teamsWithDate)
+    {
+        Console.WriteLine($"{team.Name} - {team.CreatedDate}");
+    }
+}
+
+void OrderByItems()
+{
+    var ascendingItems = context.Teams.OrderBy(q => q.Name);
+    foreach (var team in ascendingItems)
+    {
+        Console.WriteLine(team.Name);
+    }
+
+    var descendingItems = context.Teams.OrderByDescending(q => q.Name);
+    foreach (var team in descendingItems)
+    {
+        Console.WriteLine(team.Name);
+    }
+}
+
+async Task GroupByItems()
+{
+    var groupedTeams = context.Teams.
+        GroupBy(q => new { q.CreatedDate.Date, q.Name });
+
+    foreach (var group in groupedTeams)
+    {
+        Console.WriteLine(group.Key);
+        foreach (var team in group)
+        {
+            Console.WriteLine(team.Name);
+        }
+    }
+}
+async Task AggregateFunctions()
+{
+    var countAll = await context.Teams.CountAsync();
+    Console.WriteLine(countAll);
+
+    var countContainsL = await context.Teams.CountAsync(q => q.Name.Contains("l"));
+    Console.WriteLine(countContainsL);
+
+    var min = await context.Teams.MinAsync(q => q.TeamId);
+    Console.WriteLine(min);
+
+    var max = await context.Teams.MaxAsync(q => q.TeamId);
+    Console.WriteLine(max);
+
+    var avg = await context.Teams.AverageAsync(q => q.TeamId);
+    Console.WriteLine(avg);
+
+    var sum = await context.Teams.SumAsync(q => q.TeamId);
+    Console.WriteLine(sum);
+}
+
+async Task GetAllTeams()
+{
+    var teams = await context.Teams.ToListAsync();
+
+    foreach (var team in teams)
+    {
+        Console.WriteLine(team.Name);
+    }
+}
+
+void QuerySingleRecords()
+{
+    var teamFirst = context.Teams.First();
+    Console.WriteLine(teamFirst.Name);
+
+    var teamSecond = context.Teams.First(q => q.TeamId == 2);
+    Console.WriteLine(teamSecond.Name);
+
+    var teamThird = context.Teams.FirstOrDefault(q => q.TeamId == 8);
+
+    var teamFourth = context.Teams.Find(3);
+    Console.WriteLine(teamFourth.Name);
+
+    var teamFifth = context.Teams.Single(q => q.Name == "India");
+    Console.WriteLine(teamFifth.TeamId);
+}
+
+async Task FilterMultipleRecords()
+{
+    var teams = await context.Teams.Where(q => q.TeamId < 3).ToListAsync();
+    foreach (var team in teams)
+    {
+        Console.WriteLine(team.Name);
+    }
+}
+
+async Task SearchWithWildCard()
+{
+    var teams = await context.Teams.Where(q => EF.Functions.Like(q.Name, $"%{"l"}")).ToListAsync(); //Ends with l
+    foreach (var team in teams)
+    {
+        Console.WriteLine(team.Name);
+    }
+}
--- EntityFrameworkCore.Data/FootballLeagueDbContext.cs
+++ EntityFrameworkCore.Data/FootballLeagueDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using EntityFrameworkCore.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace EntityFrameworkCore.Data
 {
@@ -7,10 +8,22 @@ namespace EntityFrameworkCore.Data
     {
         public DbSet<Team> Teams { get; set; }
         public DbSet<Coach> Coaches { get; set; }
+        public string DbPath { get; private set; }
+
+        public FootballLeagueDbContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+
+            DbPath = Path.Combine(path, "FootballLeague_EfCore.db");
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite($"Data Source= FootballLeague_EfCore.db");
+            optionsBuilder.UseSqlite($"Data Source= {DbPath}")
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
